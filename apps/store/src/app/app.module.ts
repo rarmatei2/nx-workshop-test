68243888e02af24cfa1f219ff87b326169119ec5
Rares Matei <matei.rar@gmail.com>
2020-11-12 22:21:33 +0000

    finish lab4

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     MatCardModule,
+    StoreUiSharedModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
   ],
   providers: [],
